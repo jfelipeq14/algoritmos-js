523ccdaef80ecc61a6fc93d082ba977039474eaf
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-01-26 11:22:24 -0500

    actividades para la introduccion

---
+++ 2. Introduccion/actividadTres.js
@@ -0,0 +1,1 @@
+// Hacer un algoritmo que lea el nombre de un estudiante, la cantidad de materias que cursa en el semestre, la cantidad de materias perdidas y muestre el nombre y el porcentaje de materias ganadas.
